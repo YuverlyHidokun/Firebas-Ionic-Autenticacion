4b64be69ecd486038a0b2418b79c03565eb613a8
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2024-11-08 16:35:56 -0500

    Ejercicio incompleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app'
+import {getAuth, provideAuth} from '@angular/fire/auth'
+import {getFirestore, provideFirestore} from '@angular/fire/firestore'
+import {getStorage, provideStorage} from '@angular/fire/storage'
+
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
